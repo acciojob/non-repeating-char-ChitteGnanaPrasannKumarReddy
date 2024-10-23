3481dca886659548ae1ba6540fc6dd633b1d2422
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-10-23 16:45:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function firstNonRepeatedChar(str) {
+function firstNonRepeatedChar(str) { 
+	const charCount = {};
+
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1; 
+		
+    } 
+	for (let char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    return null;
+}
+	
  // Write your code here
 }
 const input = prompt("Enter a string");
